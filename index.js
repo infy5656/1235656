1fa91b8215e624ccd3d1386f4218ccda750d957b
infy5656 <44190575+infy5656@users.noreply.github.com>
2018-10-23 13:12:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -250,15 +250,6 @@ bot.on("message", function(message) {
                 message.channel.sendMessage(":interrobang: | ***Invalid Usage***\nYou forgot to mention the user you want to bam!")
             }
             break;
-        case "purge":
-            const deleteCount = parseInt(args[0], 10);
-
-            if (!deleteCount || deleteCount < 1 || deleteCount > 100)
-               return message.reply(":interrobang: | ***Invalid Usage***\n*Please provide a number between 1 and 100 for the number of messages to delete.*");
-            
-            const fetched = await message.channel.fetchMessages({limit : deleteCount});
-            message.channel.bulkDelete(fetched)
-              .catch(error => message.reply(`:interrobang: | ***Error***\n*Couldn't delete messages because of: ${error}*`));
     default:
         message.channel.sendMessage(":interrobang: | ***Invalid Command***\n*Please check if you had any typo!*");
     }
